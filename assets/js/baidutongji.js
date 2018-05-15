0bd80682704110c4074e924aa128f0c957dbba33
ZhanghaoH <243382542@qq.com>
2018-05-15 10:11:14 +0800

    百度统计

---
+++ assets/js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cd643ed6983cbf4613b420c77ddccca1";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
